2197f7c0f14117bb179cfc5da7de9b98a62ad646
iphydf <iphydf@users.noreply.github.com>
2015-12-16 15:55:54 +0000

    Add GC statistics to HostInfo.

--- projects/tox4j/src/test/java/im/tox/client/HostInfo.scala
+++ projects/tox4j/src/test/java/im/tox/client/HostInfo.scala
@@ -16,6 +16,10 @@ object HostInfo {
   val ipv4 = new Curl(new URL("http://64.182.208.184/"))
   val ipv6 = new Curl(new URL("http://[2604:7780:200:300::60]/"))
 
+  private def printDouble(value: Double)(out: PrintWriter): Unit = {
+    out.print(Math.round(value * 100) / 100.0)
+  }
+
   private def printMemory(memory: Double)(out: PrintWriter): Unit = {
     val (count, unit) =
       if (memory > 1024 * 1024 * 1024) {
@@ -28,14 +32,25 @@ object HostInfo {
         (memory, "B")
       }
 
-    out.print(Math.round(count * 100) / 100.0)
+    printDouble(count)(out)
     out.println(unit)
+  }
+
+  private def printGcStatistics(out: PrintWriter): Unit = {
+    out.println("GC statistics:")
+    val uptime = ManagementFactory.getRuntimeMXBean.getUptime
+    val gcMxs = ManagementFactory.getGarbageCollectorMXBeans.iterator()
+    while (gcMxs.hasNext) {
+      val gcMx = gcMxs.next()
+      out.print("  "); out.print(gcMx.getName); out.println(":")
+      out.print("    Collection count: "); out.print(gcMx.getCollectionCount); out.println()
+      out.print("    Collection time:  "); out.print(gcMx.getCollectionTime)
+      out.print(" (ratio="); printDouble(gcMx.getCollectionTime.toDouble / uptime)(out); out.println(")")
+    }
   }
 
   def printSystemInfo(out: PrintWriter): Unit = {
     val runtime = Runtime.getRuntime
-    val memory = ManagementFactory.getMemoryMXBean
-    val memoryUsage = memory.getHeapMemoryUsage
 
     out.println(TestClient.toString)
     out.print("  Start time:           "); out.println(startTime)
@@ -44,9 +59,11 @@ object HostInfo {
     out.print("  Max memory:           "); printMemory(runtime.maxMemory)(out)
     out.print("  Total memory:         "); printMemory(runtime.totalMemory)(out)
     out.print("  Used (total - free):  "); printMemory(runtime.totalMemory - runtime.freeMemory)(out)
-    out.print("  Usage (MemoryUsage):  "); out.println(memoryUsage)
     out.print("  IPv4 address:         "); out.println(HostInfo.ipv4)
     out.print("  IPv6 address:         "); out.println(HostInfo.ipv6)
+    out.println()
+
+    printGcStatistics(out)
   }
 
 }
