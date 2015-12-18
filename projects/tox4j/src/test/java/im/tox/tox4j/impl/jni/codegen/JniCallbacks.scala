160a747b0f41e0eee9c1f248c2a8fd20fb43d25a
iphydf <iphydf@users.noreply.github.com>
2015-12-18 02:51:31 +0000

    Make JniCallbacks more robust.

--- projects/tox4j/src/test/java/im/tox/tox4j/impl/jni/codegen/JniCallbacks.scala
+++ projects/tox4j/src/test/java/im/tox/tox4j/impl/jni/codegen/JniCallbacks.scala
@@ -5,39 +5,31 @@ import im.tox.tox4j.core.callbacks.ToxCoreEventAdapter
 import im.tox.tox4j.impl.jni.codegen.NameConversions.{cxxTypeName, cxxVarName, javaVarName}
 import im.tox.tox4j.impl.jni.codegen.cxx.Ast._
 
-import scala.util.{Failure, Success, Try}
-
 object JniCallbacks extends CodeGenerator {
 
-  def generateCallbacks(clazz: Class[_]): TranslationUnit = {
-    clazz.getDeclaredMethods
-      .sortBy(method => method.getName)
-      .flatMap { method =>
-        val baseType = clazz.getInterfaces.toList.flatMap { interface =>
-          Try(interface.getMethod(method.getName, method.getParameterTypes: _*)) match {
-            case Failure(_)          => Nil
-            case Success(baseMethod) => List(baseMethod.getDeclaringClass): List[Class[_]]
-          }
-        } match {
-          case interface :: Nil =>
-            interface
-          case multiple =>
-            val names = multiple.map(_.getSimpleName).mkString(", ")
-            sys.error(s"Multiple callback interfaces provide the same method '${method.getName}': $names")
-        }
+  def getAllInterfaces(clazz: Class[_]): List[Class[_]] = {
+    (Option(clazz.getSuperclass).toList.flatMap(getAllInterfaces)
+      ++ clazz.getInterfaces
+      ++ clazz.getInterfaces.flatMap(getAllInterfaces))
+  }
 
+  def generateCallbacks(clazz: Class[_]): TranslationUnit = {
+    getAllInterfaces(clazz).filter(_.getSimpleName.endsWith("Callback"))
+      .sortBy(_.getSimpleName)
+      .flatMap { interface =>
         val expectedMethodName = {
-          val name = cxxTypeName(baseType.getSimpleName)
+          val name = cxxTypeName(interface.getSimpleName)
           javaVarName(name.substring(0, name.lastIndexOf('_')).toLowerCase)
         }
 
-        assert(
-          method.getName == expectedMethodName,
-          s"Method name ${method.getName} does not match its interface name ${baseType.getName}."
-        )
+        val method = interface.getDeclaredMethods.filter(_.getName == expectedMethodName) match {
+          case Array()             => sys.error(s"Callback interfaces $interface does not provide a method '$expectedMethodName'")
+          case Array(singleMethod) => singleMethod
+          case methods             => sys.error(s"Callback interfaces $interface contains multiple overloads for '$expectedMethodName'")
+        }
 
         Seq(
-          Comment(baseType.getName + "#" + expectedMethodName),
+          Comment(interface.getName + "#" + expectedMethodName),
           MacroCall(FunCall(Identifier("CALLBACK"), Seq(Identifier(cxxVarName(method.getName)))))
         )
       }
