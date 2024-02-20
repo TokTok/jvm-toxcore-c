6f83cc5a385edab00f6143040c3e05bea98ea6dc
iphydf <iphydf@users.noreply.github.com>
2024-02-20 23:07:47 +0000

    perf: Move to protobuf-lite.
    7.3M -> 4.8M for the JNI library.

--- docker/jvm.Dockerfile
+++ docker/jvm.Dockerfile
@@ -17,9 +17,27 @@ RUN apt-get update \
  && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /work/jvm-toxcore-c
+# Rarely changing external dependencies.
 COPY scripts/ /work/jvm-toxcore-c/scripts/
 RUN scripts/build-host "$PWD/_install/host/protobuf.stamp" "-j$(nproc)"
 RUN scripts/build-host "$PWD/_install/host/toxcore.stamp" "-j$(nproc)"
+
+# Native code, changes less frequently.
 COPY lib/src/main/cpp/ /work/jvm-toxcore-c/lib/src/main/cpp/
 COPY lib/src/main/proto/ /work/jvm-toxcore-c/lib/src/main/proto/
-RUN scripts/build-host "$PWD/_install/host/tox4j.stamp" "-j$(nproc)"
+RUN touch "$PWD/_install/host/.stamp" \
+ && touch "$PWD/_install/host/libsodium.stamp" \
+ && touch "$PWD/_install/host/libvpx.stamp" \
+ && touch "$PWD/_install/host/opus.stamp" \
+ && touch "$PWD/_install/host/protobuf.stamp" \
+ && touch "$PWD/_install/host/toxcore.stamp" \
+ && scripts/build-host "$PWD/_install/host/tox4j.stamp" "-j$(nproc)"
+RUN ["ls", "-lh", "/work/jvm-toxcore-c/_install/host/lib/libtox4j-c.so"]
+
+# Java/Kotlin code changes a lot.
+COPY gradlew gradle.properties settings.gradle.kts /work/jvm-toxcore-c/
+COPY gradle /work/jvm-toxcore-c/gradle/
+COPY lib/ /work/jvm-toxcore-c/lib/
+ENV LD_LIBRARY_PATH=/work/jvm-toxcore-c/_install/host/lib
+ENV PATH=/work/jvm-toxcore-c/_install/host/bin:$PATH
+RUN ./gradlew build
--- scripts/dependencies.mk
+++ scripts/dependencies.mk
@@ -1,6 +1,8 @@
 PRE_RULE = (echo "=== Building $@ ==="; ls -ld $@; true) && ls -ld $+
 POST_RULE = ls -ld $@
 
+GIT_CLONE = git clone --depth=1 --recurse-submodules --shallow-submodules
+
 $(BUILDDIR)/tox4j/Makefile: $(CURDIR)/lib/src/main/cpp/CMakeLists.txt $(TOOLCHAIN_FILE) $(foreach i,protobuf toxcore,$(PREFIX)/$i.stamp)
 	@$(PRE_RULE)
 	mkdir -p $(@D)
@@ -17,14 +19,13 @@ $(PREFIX)/tox4j.stamp: $(BUILDDIR)/tox4j/Makefile
 # protobuf
 
 $(SRCDIR)/protobuf:
-	git clone --depth=1 --branch=v3.11.1 https://github.com/google/protobuf $@
+	$(GIT_CLONE) --branch=v3.24.4 https://github.com/protocolbuffers/protobuf $@
 
 $(PREFIX)/protobuf.stamp: $(SRCDIR)/protobuf $(TOOLCHAIN_FILE) $(PROTOC)
 	@$(PRE_RULE)
-	cd $< && autoreconf -fi
 	mkdir -p $(BUILDDIR)/$(notdir $<)
-	cd $(BUILDDIR)/$(notdir $<) && $(SRCDIR)/$(notdir $<)/configure $($(notdir $<)_CONFIGURE)
-	$(MAKE) -C $(BUILDDIR)/$(notdir $<) install V=0
+	cd $(BUILDDIR)/$(notdir $<) && cmake $(SRCDIR)/$(notdir $<) $($(notdir $<)_CONFIGURE)
+	$(MAKE) -C $(BUILDDIR)/$(notdir $<) install
 	mkdir -p $(@D) && touch $@
 	@$(POST_RULE)
 
@@ -35,7 +36,7 @@ $(SRCDIR)/toxcore:
 	if [ -e ../c-toxcore ]; then								\
 	  ln -s $(realpath ../c-toxcore) $@;							\
 	else											\
-	  git clone --depth=1 --recurse-submodules https://github.com/TokTok/c-toxcore $@;	\
+	  $(GIT_CLONE) https://github.com/TokTok/c-toxcore $@;	\
 	fi
 
 $(PREFIX)/toxcore.stamp: $(foreach f,$(shell cd $(SRCDIR)/toxcore && git ls-files),$(SRCDIR)/toxcore/$f)
@@ -51,7 +52,7 @@ $(PREFIX)/toxcore.stamp: $(SRCDIR)/toxcore $(TOOLCHAIN_FILE) $(foreach i,libsodi
 # libsodium
 
 $(SRCDIR)/libsodium:
-	git clone --depth=1 --branch=1.0.18 https://github.com/jedisct1/libsodium $@
+	$(GIT_CLONE) --branch=1.0.18 https://github.com/jedisct1/libsodium $@
 
 $(PREFIX)/libsodium.stamp: $(SRCDIR)/libsodium $(TOOLCHAIN_FILE)
 	@$(PRE_RULE)
@@ -66,7 +67,7 @@ $(PREFIX)/libsodium.stamp: $(SRCDIR)/libsodium $(TOOLCHAIN_FILE)
 # opus
 
 $(SRCDIR)/opus:
-	git clone --depth=1 https://github.com/xiph/opus $@
+	$(GIT_CLONE) https://github.com/xiph/opus $@
 
 $(PREFIX)/opus.stamp: $(SRCDIR)/opus $(TOOLCHAIN_FILE)
 	@$(PRE_RULE)
@@ -81,7 +82,7 @@ $(PREFIX)/opus.stamp: $(SRCDIR)/opus $(TOOLCHAIN_FILE)
 # libvpx
 
 $(SRCDIR)/libvpx:
-	git clone --depth=1 --branch=v1.6.0 https://github.com/webmproject/libvpx $@
+	$(GIT_CLONE) --branch=v1.6.0 https://github.com/webmproject/libvpx $@
 	cd $@ && patch -p1 < $(CURDIR)/scripts/patches/libvpx.patch
 
 $(PREFIX)/libvpx.stamp: $(SRCDIR)/libvpx $(TOOLCHAIN_FILE)
