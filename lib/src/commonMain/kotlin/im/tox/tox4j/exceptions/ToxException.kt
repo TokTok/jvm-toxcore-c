c9d4ac8b50804971d6397973b810d917a754a68a
iphydf <iphydf@users.noreply.github.com>
2024-02-21 15:32:22 +0000

    test: Add ktlint test to bazel build.

--- lib/src/commonMain/kotlin/im/tox/tox4j/exceptions/ToxException.kt
+++ lib/src/commonMain/kotlin/im/tox/tox4j/exceptions/ToxException.kt
@@ -1,11 +1,13 @@
 package im.tox.tox4j.exceptions
 
-abstract class ToxException constructor(val code: Enum<*>, private val extraMessage: String) :
-    Exception(extraMessage) {
-        final override val message: String
-            get() =
-                when (extraMessage) {
-                    "" -> "Error code: " + code.name
-                    else -> extraMessage + ", error code: " + code.name
-                }
-    }
+abstract class ToxException(
+    val code: Enum<*>,
+    private val extraMessage: String,
+) : Exception(extraMessage) {
+    final override val message: String
+        get() =
+            when (extraMessage) {
+                "" -> "Error code: " + code.name
+                else -> extraMessage + ", error code: " + code.name
+            }
+}
