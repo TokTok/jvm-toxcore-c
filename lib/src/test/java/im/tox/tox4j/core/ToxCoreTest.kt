6bd2b33d3dbc6c8ecb774c008cbdf643eac4d8e9
iphydf <iphydf@users.noreply.github.com>
2025-01-07 23:10:49 +0000

    test: Add a live node bootstrap test.

--- lib/src/test/java/im/tox/tox4j/core/ToxCoreTest.kt
+++ lib/src/test/java/im/tox/tox4j/core/ToxCoreTest.kt
@@ -1,7 +1,9 @@
 package im.tox.tox4j.core
 
 import im.tox.tox4j.core.callbacks.ToxCoreEventListener
+import im.tox.tox4j.core.data.Port
 import im.tox.tox4j.core.data.ToxFriendNumber
+import im.tox.tox4j.core.data.ToxPublicKey
 import im.tox.tox4j.core.enums.ToxConnection
 import im.tox.tox4j.core.exceptions.ToxBootstrapException
 import im.tox.tox4j.core.options.ToxOptions
@@ -11,20 +13,48 @@ import kotlin.test.assertFailsWith
 
 @kotlin.ExperimentalStdlibApi
 class ToxCoreTest {
+    private val options = ToxOptions(localDiscoveryEnabled = false)
+
     @Test
     fun bootstrap_withWrongHost_shouldFail() =
         runTox {
-            val tox = newToxCore(ToxOptions())
+            val tox = newToxCore(options)
             assertFailsWith<ToxBootstrapException> {
                 tox.bootstrap("host-does-not-exist", tox.getUdpPort, tox.getDhtId)
             }
         }
 
     @Test
+    fun bootstrap_withCorrectHost_shouldSucceed() =
+        runTox {
+            val tox = newToxCore(options)
+            tox.bootstrap(
+                "tox.abilinski.com",
+                Port(33445.toUShort()),
+                ToxPublicKey(fromHex("10C00EB250C3233E343E2AEBA07115A5C28920E9C8D29492F6D00B29049EDC7E")),
+            )
+
+            var connected = false
+
+            val isConnected =
+                object : ToxCoreEventListener<Boolean> {
+                    override fun selfConnectionStatus(
+                        connectionStatus: ToxConnection,
+                        state: Boolean,
+                    ) = connectionStatus != ToxConnection.NONE
+                }
+
+            while (!connected) {
+                connected = tox.iterate(isConnected, connected)
+                delay(tox.iterationInterval.toLong())
+            }
+        }
+
+    @Test
     fun addFriendNorequest_shouldConnectTwoToxes() =
         runTox {
-            val tox1 = newToxCore(ToxOptions())
-            val tox2 = newToxCore(ToxOptions())
+            val tox1 = newToxCore(options)
+            val tox2 = newToxCore(options)
 
             tox2.bootstrap("localhost", tox1.getUdpPort, tox1.getDhtId)
 
@@ -49,4 +79,14 @@ class ToxCoreTest {
                 delay(tox1.iterationInterval.toLong())
             }
         }
+
+    private companion object {
+        fun fromHex(hex: String): ByteArray {
+            val bytes = ByteArray(hex.length / 2)
+            for (i in bytes.indices) {
+                bytes[i] = hex.substring(i * 2, i * 2 + 2).toInt(16).toByte()
+            }
+            return bytes
+        }
+    }
 }
